c14c19dcfc655628a746b6aa0466157686e98e44
Alexandre MORAY <alexandre.moray@epfedu.fr>
2020-08-17 22:50:19 +0200

    feat: Added tail and food

--- game/game.js
+++ game/game.js
@@ -2,17 +2,21 @@ const root = document.getElementById("root");
 const GAME_TERRAIN_WIDTH = 10; // cells
 const GAME_SPEED = 200; // ms
 
-const run = () => {
+(() => {
     const gameContainer = init();
     root.appendChild(gameContainer);
-    start(gameContainer).then();
-};
+    start().then();
+})();
 
-run();
-
+/**
+ * Wait the amount of milliseconds given.
+ * @param time
+ * @returns {Promise<*>}
+ */
 async function sleep(time) {
     return new Promise(r => setTimeout(r, time));
 }
+
 function init() {
     const container = document.createElement("div");
     container.id="gameContainer";
@@ -29,10 +33,21 @@ function init() {
     return container;
 }
 
+/**
+ * Returns the position of the element passed as an argument.
+ * @param element
+ * @returns {{x: *, y: *}}
+ */
 function elementToPos(element) {
     const coords = element.id.split(":").map(coord => parseInt(coord));
     return {x: coords[0], y: coords[1]};
 }
+
+/**
+ * Returns the Element corresponding to the given position.
+ * @param pos
+ * @returns {HTMLElement|undefined}
+ */
 function posToElement(pos) {
     if(pos.x >= 0 && pos.y >= 0 && pos.x < GAME_TERRAIN_WIDTH && pos.y < GAME_TERRAIN_WIDTH) {
         return document.getElementById(`${pos.x}:${pos.y}`);
@@ -40,6 +55,13 @@ function posToElement(pos) {
         return undefined;
     }
 }
+
+/**
+ * Returns the position of the next head cell, given the actual head and the direction.
+ * @param position
+ * @param direction
+ * @returns {{x: *, y: *}}
+ */
 function nextCellPos(position, direction) {
     return {
         x: position.x + direction.x,
@@ -47,37 +69,73 @@ function nextCellPos(position, direction) {
     }
 }
 
-async function start(container) {
-    let lastDirection = {x: 0, y: 0, used: false};
-    let head = document.getElementById("0:0");
+async function start() {
+    let actualDirection = {
+        x: 0,
+        y: 0,
+        used: false
+    };
+    let snake = {
+        head: document.getElementById("0:3"),
+        tail: []
+    };
+
+    snake.tail.push({x: 0, y: 2});
+    snake.tail.push({x: 0, y: 1});
+    snake.tail.push({x: 0, y: 0});
+    throwFood();
 
     document.addEventListener(
         'keydown',
         (e) => {
-        lastDirection = changeDirection(e, lastDirection);
+        actualDirection = changeDirection(e, actualDirection);
     });
 
     do {
-        head = move(head, lastDirection);
-        lastDirection.used = true;
-        await sleep(200);
+        snake = move(snake, actualDirection);
+        actualDirection.used = true;
+        await sleep(GAME_SPEED);
     } while(true);
 }
 
-function move(head, direction) {
-    const previousPos = elementToPos(head);
-    const nextPos = nextCellPos(previousPos, direction);
-    const newHead = posToElement(nextPos);
+function move(snake, direction) {
+    const previousPos = elementToPos(snake.head);
+    const newHead = posToElement(nextCellPos(previousPos, direction));
 
     if(newHead) {
-        head.classList.remove("red");
-        newHead.classList.add("red");
-        return newHead;
-    } else {
-        return head;
+        snake.head = newHead;
+        snake.tail.push(previousPos);
+        snake.head.classList.add("snake");
+
+        if(!snake.head.classList.contains("food")) {
+            posToElement(snake.tail.shift()).classList.remove("snake");
+        } else {
+            snake.head.classList.remove("food");
+            throwFood();
+        }
     }
+
+    return snake;
 }
 
+function throwFood() {
+    let x, y, cell;
+
+    do {
+        x = Math.floor(Math.random()*GAME_TERRAIN_WIDTH);
+        y = Math.floor(Math.random()*GAME_TERRAIN_WIDTH);
+        cell = document.getElementById(`${x}:${y}`);
+    } while(!cell || cell.classList.contains("snake"));
+
+    cell.classList.add("food");
+}
+
+/**
+ * Returns new direction depending on the key event given.
+ * @param e
+ * @param lastDirection
+ * @returns {{x: number, y: number, used: boolean}}
+ */
 function changeDirection(e, lastDirection) {
     let newDirection = {x: 0, y: 0, used: false};
 
